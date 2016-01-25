e43e6deb189c8a0f37ff930cdbd883dcf0f38fc9
Patrick O'Neill <pon2@umbc.edu>
2016-01-25 00:00:41 -0500

    switch out distutils for setuptools, add tqdm dependency

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(name='formosa',
       version='0.0.1',
       author="Patrick O'Neill",
       author_email="pon2@umbc.edu",
       py_modules=['formosa','maxent_sampling','uniform_sampling', 'formosa_utils'],
+      install_requires=['tqdm']
       )
