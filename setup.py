d9c34342ddfa18f1d4f4ae7b2703e0eb2477cd27
Patrick O'Neill <pon2@umbc.edu>
2016-01-24 23:30:44 -0500

    init commit setup

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='formosa',
+      version='0.0.1',
+      author="Patrick O'Neill",
+      author_email="pon2@umbc.edu",
+      py_modules=['formosa','maxent_sampling','uniform_sampling', 'formosa_utils'],
+      )
