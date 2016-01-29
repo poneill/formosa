386fce859920c0dda3e1236ad0fb332c4ca8ca75
Patrick O'Neill <pon2@umbc.edu>
2016-01-29 15:41:13 -0500

    import motif_ic

--- formosa.py
+++ formosa.py
@@ -4,6 +4,7 @@ from maxent_sampling import maxent_motif, maxent_motifs
 from maxent_sampling import spoof_maxent_motif, spoof_maxent_motifs
 from uniform_sampling import uniform_motif, uniform_motifs            
 from uniform_sampling import spoof_uniform_motif, spoof_uniform_motifs
+from formosa_utils import motif_ic
 import sys
 import argparse
 
