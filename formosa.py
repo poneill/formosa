564495c7db232ec5ef7309a429376925f4d13092
Patrick O'Neill <pon2@umbc.edu>
2016-01-28 23:01:43 -0500

    hashbang

--- formosa.py
+++ formosa.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from maxent_sampling import maxent_motif, maxent_motifs
 from maxent_sampling import spoof_maxent_motif, spoof_maxent_motifs
 from uniform_sampling import uniform_motif, uniform_motifs            
