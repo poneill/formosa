716dc173605e76d19452b2863d0c925bebc0405c
Patrick O'Neill <pon2@umbc.edu>
2016-02-23 19:12:12 -0500

    add correction option to motif_mi

--- formosa_utils.py
+++ formosa_utils.py
@@ -3,6 +3,7 @@ from scipy.special import gammaln
 import numpy as np
 import bisect
 import random
+from math import pi, log, exp
 
 def h(ps):
     """compute entropy (in bits) of a probability distribution ps"""
@@ -71,7 +72,7 @@ def concat(xss):
 
 def permute(xs):
     """Return a random permutation of xs"""
-    #return np.random.permutation(xs)
+    #return list(np.random.permutation(xs))
     xs_ = list(xs[:])
     random.shuffle(xs_)
     return xs_
@@ -152,9 +153,9 @@ def sample_until(p,sampler,n,progress_bar=True):
 def inrange(M,I,epsilon):
     return abs(motif_ic(M)-I) < epsilon
 
-def motif_mi(motif):
+def motif_mi(motif, correct=True):
     cols = transpose(motif)
-    return sum([mi(col1,col2) for (col1,col2) in choose2(cols)])
+    return sum([mi(col1,col2, correct=correct) for (col1,col2) in choose2(cols)])
 
 def mi(xs,ys,correct=True):
     """Compute mutual information (in bits) of samples from two
@@ -166,16 +167,17 @@ def mi(xs,ys,correct=True):
     hxy = entropy(zip(xs,ys),correct=correct,alphabet_size=16)
     return hx + hy - hxy
 
-def mi_test_cols(xs, ys, alpha=0.05, trials=1000, perm_test=True):
+def mi_test_cols(xs, ys, alpha=None, trials=1000):
     """using permutation test, do (1-sided) significance test for MI.  If
     permute, compare to random permutation, otherwise random site"""
     N = len(xs)
     obs_mi = mi(xs,ys)
-    nullx = lambda : permute(xs) if perm_test else random_site(N)
-    nully = lambda : permute(ys) if perm_test else random_site(N)
-    perm_replicates = (mi(nullx(),nully()) for i in xrange(trials))
+    perm_replicates = (mi(permute(xs),permute(ys)) for i in xrange(trials))
     perc = len(filter(lambda x:x >= obs_mi,perm_replicates))/float(trials)
-    return perc < alpha
+    if alpha:
+        return perc < alpha
+    else:
+        return perc
 
 def count(p,xs):
     """Count number of xs satisfying p"""
@@ -187,3 +189,119 @@ def choose2(xs, gen=False):
         return ((x1, x2) for i, x1 in enumerate(xs) for x2 in xs[i+1:])
     else:
         return [(x1, x2) for i, x1 in enumerate(xs) for x2 in xs[i+1:]]
+
+def sorted_indices(xs):
+    """Return a list of indices that puts xs in sorted order.
+    E.G.: sorted_indices([40,10,30,20]) => [1,3,2,0]"""
+    return [i for (i,v) in sorted(enumerate(xs),key=lambda(i,v):v)]
+
+def subst(xs,ys,i):
+    """Substitute substring ys in xs, starting at i"""
+    if not (type(ys) is list or type(ys) is str):
+        ys = [ys]
+    return xs[:i] + ys + xs[i+len(ys):]
+
+def mutate_site(site):
+    i = random.randrange(len(site))
+    b = site[i]
+    new_b = random.choice([c for c in "ACGT" if not c == b])
+    return subst(site,new_b,i)
+
+def score_seq(matrix,seq):
+    #base_dict = {'A':0,'C':1,'G':2,'T':3}
+    def base_dict(b):
+        if b <= "C":
+            if b == "A":
+                return 0
+            else:
+                return 1
+        elif b == "G":
+            return 2
+        else:
+            return 3
+    ans = 0
+    for i in xrange(len(seq)):
+        ans += matrix[i][base_dict(seq[i])]
+    return ans
+
+def seq_scorer(matrix):
+    """accept matrix, return a function scoring sites"""
+    # when score_seq JUST ISN'T FAST ENOUGH
+    base_dicts = [{b:row[j] for j,b in enumerate("ACGT")} for row in matrix]
+    def f(site):
+        ans = 0
+        for i in xrange(len(site)):
+            ans += base_dicts[i][site[i]]
+        return ans
+    return f
+
+def argmin(xs):
+    i,x = min(enumerate(xs),key= lambda (i,x):x)
+    return i
+
+def argmax(xs):
+    i,x = max(enumerate(xs),key= lambda (i,x):x)
+    return i
+
+def rslice(xs,js):
+    return [xs[j] for j in js]
+
+def sigma_from_matrix(matrix):
+    """given a GLE matrix, estimate standard deviation of cell weights,
+    correcting for bias of sd estimate.  See:
+    https://en.wikipedia.org/wiki/Unbiased_estimation_of_standard_deviation
+    """
+    c = 2*sqrt(2/(3*pi))
+    return mean(map(lambda x:sd(x,correct=True),matrix))/c
+
+def pssm_from_motif(motif, pc=1):
+    psfm = psfm_from_motif(motif, pc)
+    return [[log(f/0.25,2) for f in row] for row in psfm]
+
+def psfm_from_motif(motif,pc=1):
+    n = float(len(motif))
+    cols = transpose(motif)
+    return [[(col.count(b) + pc)/(n+4*pc) for b in "ACGT"] for col in cols]
+
+def normalize(xs):
+    total = float(sum(xs))
+    return [x/total for x in xs]
+
+def sample_matrix(L,sigma):
+    return [[random.gauss(0,sigma) for j in range(4)] for i in range(L)]
+
+def approx_mu(matrix, copies, G=5*10**6):
+    Zb = Zb_from_matrix(matrix, G)
+    return log(copies) - log(Zb)
+
+def Zb_from_matrix(matrix,G):
+    """calculate partition function"""
+    L = len(matrix)
+    Zb_hat = prod(sum(exp(-ep) for ep in col) for col in matrix)/(4**L)
+    return G * Zb_hat
+
+def prod(xs):
+    acc = 1
+    for x in xs:
+        acc *= x
+    return acc
+
+def occupancy(site, matrix, copies, G=5*10**6):
+    mu = approx_mu(matrix, copies, G=G)
+    ep = score_seq(matrix, site)
+    return 1/(1+exp(ep-mu))
+
+def occupancies(motif, copy_factor=10, G=5*10**6):
+    matrix = matrix_from_motif(motif)
+    copies = copy_factor * len(motif)
+    mu = approx_mu(matrix, copies, G=G)
+    eps = [score_seq(matrix, site) for site in motif]
+    return [1/(1+exp(ep-mu)) for ep in eps]
+        
+def matrix_from_motif(seqs,pc=1):
+    cols = transpose(seqs)
+    N = float(len(seqs))
+    raw_mat = [[-log((col.count(b)+pc)/(N+4*pc)) for b in "ACGT"] for col in cols]
+    # now normalize each column by the average value
+    avg = mean(map(mean,raw_mat))
+    return [[x-avg for x in row] for row in raw_mat]
