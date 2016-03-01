1103e0f1ae35590c830d3fd533aaef08d85c9ccd
Patrick O'Neill <pon2@umbc.edu>
2016-03-01 14:48:14 -0500

    don't correct motif_mi

--- formosa_utils.py
+++ formosa_utils.py
@@ -153,7 +153,7 @@ def sample_until(p,sampler,n,progress_bar=True):
 def inrange(M,I,epsilon):
     return abs(motif_ic(M)-I) < epsilon
 
-def motif_mi(motif, correct=True):
+def motif_mi(motif, correct=False):
     cols = transpose(motif)
     return sum([mi(col1,col2, correct=correct) for (col1,col2) in choose2(cols)])
 
